5b1cb91ca63a4ffdd037bc5fc2437ef326eb7b7b
wanglin <137789954@qq.com>
2024-04-03 10:03:37 +0800

    feat: 环境变量

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,11 @@
+"use client"
 
-import './page.less';
+// import './page.less';
 
 export default function Login() {
+
+  console.log(process.env.NEXT_PUBLIC_ENV, 'process.env.NEXT_PUBLIC_ENV')
+
   return (
    <div className='login'>
       登录
